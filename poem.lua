553c8391b6baf51d5b67c1f14f4fd4860b3bf48b
MoeX <54111871+MengXin001@users.noreply.github.com>
2020-08-06 16:15:15 +0800

    随机古诗

---
+++ poem.lua
@@ -0,0 +1,36 @@
+local log = require("log")
+local Api = require("coreApi")
+local json = require("json")
+local http = require("http")
+function ReceiveFriendMsg(CurrentQQ, data)
+    return 1
+end
+function ReceiveGroupMsg(CurrentQQ, data)
+if string.find(data.Content, "poem") then
+        response, error_message =
+            http.request(
+            "GET",
+            "https://v1.jinrishici.com/all"
+        )
+        local html = response.body
+        local str = html
+		local j = json.decode(str)
+            ApiRet =
+                Api.Api_SendMsg(
+                CurrentQQ,
+                {
+                    toUser = data.FromGroupId,
+                    sendToType = 2,
+                    sendMsgType = "TextMsg",
+                    groupid = 0,
+                    content = "“" ..j.content.. "”\n" ..j.author.. "《"..j.origin.. "》",
+                    atUser = 0
+                }
+            )
+            html = nil
+            str = nil
+            j = nil
+            log.notice("From Lua SendMsg Ret-->%d", ApiRet.Ret)
+        end
+    return 1
+end
